c8f7adc8d4a65568f841d8617fa3b52ee096d6cc
4074 <137657976@qq.com>
2018-01-12 19:01:43 +0800

    draw line

--- MagicChart/LineChart.swift
+++ MagicChart/LineChart.swift
@@ -9,13 +9,14 @@
 import UIKit
 
 class LineChart: UIView {
-
-    var dataSet: [LineChartDataSet] = [] {
+    
+    var dataSource: LineChartDataSource = LineChartDataSource() {
         didSet {
-            render()
+            self.reset()
+            self.render()
         }
     }
-    var dataSetLayer: [CAShapeLayer] = []
+    var dataSetLayer: [(line: CAShapeLayer, point: CAShapeLayer?)] = []
     
     var rangeType: (minimum: MagicChartAxisRangeType, maximum: MagicChartAxisRangeType) = (.zero, .auto)
     var range: (minimum: Double, maximum: Double)?
@@ -24,10 +25,13 @@ class LineChart: UIView {
     
     var origin: CGPoint = CGPoint(x: 0, y: 0)
     var inset: UIEdgeInsets = UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 16)
+    var layerFrame: CGRect = .zero
     
     var axisLayer: (x: CAShapeLayer, y: CAShapeLayer)?
     var axisLineWidth: (x: CGFloat, y: CGFloat) = (1, 1)
     var axisLineColor: (x: UIColor, y: UIColor) = (.lightGray, .lightGray)
+    
+    var graphicsContext: CGContext?
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -38,10 +42,33 @@ class LineChart: UIView {
     }
     
     func render() {
-        origin = CGPoint(x: inset.left, y: self.frame.size.height - inset.bottom)
+        origin = CGPoint(x: 0, y: self.frame.size.height - inset.top - inset.bottom)
         range = getYAxisRange()
+        layerFrame = CGRect(
+            x: inset.left,
+            y: inset.top,
+            width: self.frame.size.width - inset.left - inset.right,
+            height: self.frame.size.height - inset.top - inset.bottom
+        )
+        
         drawAxisLine()
         drawSetLine()
+    }
+    
+    func reset() {
+        if let axisLayer = self.axisLayer {
+            axisLayer.x.removeFromSuperlayer()
+            axisLayer.y.removeFromSuperlayer()
+            self.axisLayer = nil
+        }
+        
+        for group in dataSetLayer {
+            group.line.removeFromSuperlayer()
+            if let point = group.point {
+                point.removeFromSuperlayer()
+            }
+        }
+        dataSetLayer.removeAll()
     }
     
     func drawSetLine() {
@@ -49,76 +76,86 @@ class LineChart: UIView {
             return
         }
         
-        let size = self.frame.size
+        CATransaction.begin()
         
-        for (index, set) in dataSet.enumerated() {
-            let layer = CAShapeLayer()
-            layer.frame = CGRect(
-                x: inset.left,
-                y: inset.top,
-                width: size.width - inset.left - inset.right,
-                height: size.height - inset.top - inset.bottom
-            )
+        for (index, set) in dataSource.sets.enumerated() {
+            let lineLayer = CAShapeLayer()
+            let color = (set.lineColor ?? colors[index % colors.count])
+            var points = [CGPoint]()
+            
+            lineLayer.frame = layerFrame
             
             let path = UIBezierPath()
             path.lineCapStyle = .round
             path.lineJoinStyle = .round
-            path.lineWidth = set.lineWidth
             
-            for (i, v) in set.value.enumerated() {
-                let x = (Double(i) / Double(set.value.count)) * Double(layer.frame.width)
-                let y = (1 - (v / (range.maximum - range.minimum))) * Double(layer.frame.height)
-                let point = CGPoint(x: x, y: y)
-                
-                if i == 0 {
-                    path.move(to: point)
-                } else {
-                    path.addLine(to: point)
+            for (i, k) in dataSource.label.enumerated() {
+                if let v = set.value[k] {
+                    let x = (Double(i) / Double(set.value.count - 1)) * Double(layerFrame.width)
+                    let y = (1 - (v / (range.maximum - range.minimum))) * Double(layerFrame.height)
+                    let point = CGPoint(x: x, y: y)
+                    
+                    if i == 0 {
+                        path.move(to: point)
+                    } else {
+                        path.addLine(to: point)
+                    }
+                    points.append(point)
                 }
             }
             
-            layer.path = path.cgPath
-            layer.strokeColor = (set.lineColor ?? colors[index % colors.count]).cgColor
-            layer.fillColor = nil
+            lineLayer.path = path.cgPath
+            lineLayer.strokeColor = color.cgColor
+            lineLayer.lineWidth = set.lineWidth
+            lineLayer.fillColor = UIColor.clear.cgColor
+            addAnimationToLayer(lineLayer)
+            self.layer.addSublayer(lineLayer)
             
-            self.layer.addSublayer(layer)
+            if let shape = set.pointShape {
+                let pointLayer = createPointLayer(layerFrame, points: points, radius: set.pointRadius, shape: shape, color: color)
+                addAnimationToLayer(pointLayer, duration: 1.2)
+                self.layer.addSublayer(pointLayer)
+                dataSetLayer.append((lineLayer, pointLayer))
+            } else {
+                dataSetLayer.append((lineLayer, nil))
+            }
         }
         
+        CATransaction.commit()
     }
     
     func drawAxisLine() {
-        let size = self.frame.size
         
-        let xAxisLayer = createAxisLineLayer(size, end: CGPoint(x: size.width - inset.right, y: origin.y), width: axisLineWidth.x, color: axisLineColor.x)
+        let xAxisLayer = createAxisLineLayer(end: CGPoint(x: layerFrame.width, y: layerFrame.height), width: axisLineWidth.x, color: axisLineColor.x)
         self.layer.addSublayer(xAxisLayer)
         
-        let yAxisLayer = createAxisLineLayer(size, end: CGPoint(x: origin.x, y: inset.top), width: axisLineWidth.x, color: axisLineColor.x)
+        let yAxisLayer = createAxisLineLayer(end: CGPoint(x: 0, y: 0), width: axisLineWidth.x, color: axisLineColor.x)
         self.layer.addSublayer(yAxisLayer)
         
+        if !dataSource.label.isEmpty {
+            let itemWidth = layerFrame.width / (CGFloat(dataSource.label.count) - 1)
+            for (index, text) in dataSource.label.enumerated() {
+                let xAxisTextLayer = CATextLayer()
+                let width: CGFloat = 12
+                xAxisTextLayer.frame = CGRect(
+                    x: inset.left + itemWidth * CGFloat(index) - width / 2,
+                    y: inset.top + layerFrame.height + 4,
+                    width: width,
+                    height: 12
+                )
+                xAxisTextLayer.font = UIFont.systemFont(ofSize: 12)
+                xAxisTextLayer.fontSize = 12
+                xAxisTextLayer.foregroundColor = UIColor.lightGray.cgColor
+                xAxisTextLayer.string = text
+                self.layer.addSublayer(xAxisTextLayer)
+            }
+        }
+        
         axisLayer = (xAxisLayer, yAxisLayer)
-    }
-    
-    func createAxisLineLayer(_ size: CGSize, end: CGPoint, width: CGFloat, color: UIColor) -> CAShapeLayer {
-        let layer = CAShapeLayer()
-        layer.frame = CGRect(x: 0, y: 0, width: size.width, height: size.height)
-        
-        let path = UIBezierPath()
-        path.lineCapStyle = .round
-        path.lineJoinStyle = .round
-        
-        path.lineWidth = width
-        path.move(to: origin)
-        path.addLine(to: end)
-        
-        layer.path = path.cgPath
-        layer.strokeColor = color.cgColor
-        
-        return layer
     }
 }
 
 extension LineChart {
-    
     func getYAxisRange() -> (minimum: Double, maximum: Double)? {
         var minimum: Double = Double.infinity
         var maximum: Double = -Double.infinity
@@ -153,30 +190,124 @@ extension LineChart {
         var minimum: Double = Double.infinity
         var maximum: Double = -Double.infinity
         
-        for set in dataSet {
-            if let m = set.value.min() {
+        for set in dataSource.sets {
+            if let m = set.value.values.min() {
                 minimum = min(m, minimum)
             }
-            if let m = set.value.max() {
+            if let m = set.value.values.max() {
                 maximum = max(m, maximum)
             }
         }
         
         return (minimum, maximum)
     }
+    
+    func createAxisLineLayer(end: CGPoint, width: CGFloat, color: UIColor) -> CAShapeLayer {
+        let layer = CAShapeLayer()
+        layer.frame = layerFrame
+        
+        let path = UIBezierPath()
+        path.lineCapStyle = .round
+        path.lineJoinStyle = .round
+        
+        path.lineWidth = width
+        path.move(to: origin)
+        path.addLine(to: end)
+        
+        layer.path = path.cgPath
+        layer.strokeColor = color.cgColor
+        
+        return layer
+    }
+    
+    func createPointLayer(_ frame: CGRect, points: [CGPoint], radius: CGFloat, shape: MagicChartPointShape, color: UIColor) -> CAShapeLayer {
+        let layer = CAShapeLayer()
+        let path = UIBezierPath()
+        layer.frame = frame
+        
+        for point in points {
+            switch shape {
+            case .circle:
+                path.move(to: point)
+                path.addArc(withCenter: point, radius: radius/2, startAngle: 0, endAngle: CGFloat(Double.pi * 2), clockwise: true)
+                layer.lineWidth = radius
+            case .square:
+                path.move(to: point)
+                let num = CGFloat(sqrt((Double(radius) * Double(radius)) * Double.pi) / 2)
+                let x_start = point.x - num
+                let x_end = point.x + num
+                
+                path.move(to: CGPoint(x: x_start, y: point.y))
+                path.addLine(to: CGPoint(x: x_end, y: point.y))
+                
+                layer.lineWidth = num * 2
+            default:
+                break
+            }
+        }
+        
+        layer.path = path.cgPath
+        layer.strokeColor = color.cgColor
+        layer.fillColor = UIColor.clear.cgColor
+        
+        return layer
+    }
+    
+    func addAnimationToLayer(_ layer: CAShapeLayer, duration: CFTimeInterval = 1) {
+        let animation = CABasicAnimation(keyPath: "strokeEnd")
+        animation.fromValue = 0
+        animation.toValue = 1
+        animation.duration = duration
+        animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
+        layer.add(animation, forKey: "strokeEnd")
+    }
+    
+    func createDataSet(_ label: [String], groups: [[Double]], style: ((_ :LineChartDataSet, _: Int) -> Void)?) -> [LineChartDataSet] {
+        var sets = [LineChartDataSet]()
+        
+        for (index, group) in groups.enumerated() {
+            let set = LineChartDataSet()
+            if group.count != label.count {
+                continue
+            }
+            for (index, key) in label.enumerated() {
+                set.value[key] = group[index]
+            }
+            
+            if let style = style {
+                style(set, index)
+            }
+            
+            sets.append(set)
+        }
+        
+        return sets
+    }
+}
+
+class LineChartDataSource {
+    var label = [String]()
+    var sets = [LineChartDataSet]()
 }
 
 class LineChartDataSet {
-    var value = [Double]()
-    var label = [String]()
+    var value = [String: Double]()
     var lineWidth: CGFloat = 1
     var lineColor: UIColor?
+    var pointShape: MagicChartPointShape? = nil
+    var pointRadius: CGFloat = 4
 }
 
 enum MagicChartAxisRangeType {
     case auto
     case zero
     case manual
+}
+
+enum MagicChartPointShape {
+    case circle
+    case square
+    case triangle
 }
 
 
--- MagicChartDemo/MagicChartDemo/ViewController.swift
+++ MagicChartDemo/MagicChartDemo/ViewController.swift
@@ -10,19 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var lineChart: LineChart!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.e
         
-        let lineChart = LineChart()
-        view.addSubview(lineChart)
-        
-        let set = LineChartDataSet()
-        set.value = [3, 5, 7, 2, 10, 9]
+        lineChart = LineChart()
         lineChart.frame = CGRect(x: 20, y: 80, width: view.frame.width - 40, height: 200)
-        lineChart.dataSet = [set]
+        view.addSubview(lineChart)
+        refreshChart()
         
+        let button = UIButton(frame: CGRect(x: 20, y: 300, width: 80, height: 32))
+        view.addSubview(button)
+        button.setTitle("Refresh", for: .normal)
+        button.backgroundColor = .blue
+        button.addTarget(self, action: #selector(self.refreshChart), for: .touchUpInside)
+    }
+    
+    @objc func refreshChart() {
+        let dataSource = LineChartDataSource()
+        let label = ["1", "2", "3", "4", "5", "6"]
+        let sets = lineChart.createDataSet(label, groups: [[3, 5, 2, 3, 1, 9], [9, 10, 8, 7, 12, 12]]) { (set, index) in
+            if index == 0 {
+                set.pointShape = .circle
+            } else {
+                set.pointShape = .square
+            }
+        }
         
+        dataSource.label = label
+        dataSource.sets = sets
+        lineChart.dataSource = dataSource
     }
 
     override func didReceiveMemoryWarning() {
